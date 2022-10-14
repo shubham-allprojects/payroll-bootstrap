50d180ddfe1bb0357283dd16bd127763263ed828
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-10-14 13:02:36 +0530

    All Done

--- frontend/src/pages/projects.js
+++ frontend/src/pages/projects.js
@@ -3,17 +3,67 @@ import Layout from "../components/Layout"
 
 export default function projects() {
   return (
-    <div>
+    <>
       <Layout>
-        <div className="projectDiv">
+        {/* <div className="projectDiv">
           <h2 className="ProjectCls">Projects</h2>
-          <p className="ProjectCls">1. React Js <a href="#">Read More</a></p>
-          <p className="ProjectCls">2. Mongo DB <a href="#">Read More</a></p>
-          <p className="ProjectCls">3. Business Intelligence <a href="#">Read More</a></p>
-          <p className="ProjectCls">4. Application Development <a href="#">Read More</a></p>
-          <p className="ProjectCls">5. Data Architecture <a href="#">Read More</a></p>
-        </div>
+          <p className="ProjectCls">
+            1. React Js <a href="#">Read More</a>
+          </p>
+          <p className="ProjectCls">
+            2. Mongo DB <a href="#">Read More</a>
+          </p>
+          <p className="ProjectCls">
+            3. Business Intelligence <a href="#">Read More</a>
+          </p>
+          <p className="ProjectCls">
+            4. Application Development <a href="#">Read More</a>
+          </p>
+          <p className="ProjectCls">
+            5. Data Architecture <a href="#">Read More</a>
+          </p>
+        </div> */}
+        <section
+          id="projects"
+          className="projects-wrapper wrapper wrapper-padding"
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div>
+                  <ul className="list-unstyled">
+                    <li>
+                      <p>
+                        1. React Js <a href="#">Read More</a>
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        2. Mongo DB <a href="#">Read More</a>
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        3. Business Intelligence <a href="#">Read More</a>
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        4. Application Development <a href="#">Read More</a>
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        5. Data Architecture <a href="#">Read More</a>
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
-    </div>
+    </>
   )
 }
